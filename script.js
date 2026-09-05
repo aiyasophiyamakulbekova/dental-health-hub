603a3c6f70e9c745294033c428b1526e014bd1d7
aiyasophiyamakulbekova <aiyasophiya.makulbekova@gmail.com>
2026-09-05 20:34:12 +0500

    Refactor checkAnswer function for quiz results

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 
-function checkAnswer(button, correct) {
-    const result = document.getElementById("quiz-result");
+    // Quiz on prevention.html
+    if (button.parentElement.classList.contains("quiz-question")) {
+        result = button.parentElement.querySelector("p");
+    }
+
+    // Quiz on oral-health.html
+    else {
+        result = document.getElementById("quiz-result");
+    }
 
     if (correct) {
-        result.textContent = "✓ Correct! Great job!";
-        result.style.color = "#4f8062";
-    } else {
-        result.textContent = "✗ Not quite. Try again!";
-        result.style.color = "#c85f68";
-    }
-}
-
-function checkAnswer2(question, answer) {
-    const result = document.getElementById("result" + question);
-
-    if (answer === "correct") {
         result.textContent = "✓ Correct! Great job!";
         result.style.color = "#4f8062";
     } else {
